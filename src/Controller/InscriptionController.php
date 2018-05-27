2c2832a800577190c63a8687b0568066a053411b
archiafura <archiafura@icloud.com>
2018-05-27 22:10:46 +0200

    Modification InscriptionController

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -3,6 +3,11 @@
 namespace App\Controller;
 use App\Entity\Inscription;
 use Doctrine\DBAL\Types\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,12 +38,16 @@ class InscriptionController extends Controller
         $inscription = new inscription();
 
         $formulaire = $this->createFormBuilder($inscription)
-            ->add('nom', TextType::class)
-            ->add('prenom', TextType::class)
-            ->add('pseudo', TextType::class)
-            ->add('email', TextType::class)
-            ->add('adresse', TextType::class)
-            ->add('mdp', TextType::class)
+            ->add('nom', TextType::class, array('label' => 'NOM',))
+            ->add('prenom', TextType::class,array('label' => 'PRENOM',)   )
+            ->add('pseudo', TextType::class, array('label' => 'PSEUDO',))
+            ->add('email', EmailType::class, array('label' => 'EMAIL',)  )
+            ->add('adresse', TextType::class, array('label' => 'ADRESSE',) )
+            ->add('zip', NumberType::class, array('label' => 'CODE POSTAL',) )
+            ->add('mdp', PasswordType::class, array('label' => 'MOT DE PASSE',))
+            ->add('mdp', RepeatedType::class, array('label' => 'REPETER MOT DE PASSE',))
+            ->add('statut', ChoiceType::class, array('choices' => array('Particulier' => 'Particulier', 'Professionnel' =>'Professionnel',)))
+            ->add('interet', CheckboxType::class, array('label' => 'fruits', ))
             ->add('envoyer', SubmitType::class, array('label' => "Envoyer"))
             ->getForm();
 
