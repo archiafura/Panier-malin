6d1bd69291d5500b7abfaca7d329e0b651808175
Isa Vil <isa.vil@free.fr>
2018-05-28 17:23:05 +0200

    Création bdd et entités liées

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Inscription;
+
+use App\Entity\Utilisateurs;
 use Doctrine\DBAL\Types\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -21,12 +22,14 @@ class InscriptionController extends Controller
     /**
      * @Route("/inscription", name="inscription")
      */
-    public function index()
+
+        public function index()
     {
         return $this->render('inscription/index.html.twig', [
             'controller_name' => 'InscriptionController',
         ]);
     }
+
 
 
     /**
@@ -35,19 +38,21 @@ class InscriptionController extends Controller
 
     public function formulaire(Request $requete)
     {
-        $inscription = new inscription();
+        $inscription = new Utilisateurs();
 
         $formulaire = $this->createFormBuilder($inscription)
             ->add('nom', TextType::class, array('label' => 'NOM',))
             ->add('prenom', TextType::class,array('label' => 'PRENOM',)   )
-            ->add('pseudo', TextType::class, array('label' => 'PSEUDO',))
-            ->add('email', EmailType::class, array('label' => 'EMAIL',)  )
             ->add('adresse', TextType::class, array('label' => 'ADRESSE',) )
+            ->add('email', EmailType::class, array('label' => 'EMAIL',)  )
             ->add('zip', NumberType::class, array('label' => 'CODE POSTAL',) )
-            ->add('mdp', PasswordType::class, array('label' => 'MOT DE PASSE',))
-            ->add('mdp', RepeatedType::class, array('label' => 'REPETER MOT DE PASSE',))
-            ->add('statut', ChoiceType::class, array('choices' => array('Particulier' => 'Particulier', 'Professionnel' =>'Professionnel',)))
+            ->add('pseudo', TextType::class, array('label' => 'PSEUDO',))
+            ->add('password', PasswordType::class, array('label' => 'MOT DE PASSE',))
+            ->add('password', RepeatedType::class, array('label' => 'REPETER MOT DE PASSE',))
             ->add('interet', ChoiceType::class, array('choices' => array('Fruits' => array('À consommer tel quel' => 'À consommer tel quel', 'À transformer (confitures,compotes..)' => 'À transformer (confitures,compotes..)',), 'Confection de paniers' => array('Paniers de fruits' => 'Paniers de fruits', 'Paniers de légumes' => 'Paniers de légumes', 'Paniers mixte' => 'Paniers mixte',), 'Légumes' => 'Légumes', 'Artisanats' => 'Artisanats',)))
+            ->add('statut', ChoiceType::class, array('choices' => array('Particulier' => 'Particulier', 'Professionnel' =>'Professionnel',)))
+            ->add('newsletter', CheckboxType::class, array('label' => 'Inscription à la Newsletter', 'required' => false, ))
+            ->add('perimetre_activite', ChoiceType::class, array('choices' => array('30' => '30', '60' =>'60',)))
             ->add('envoyer', SubmitType::class, array('label' => "Envoyer"))
             ->getForm();
 
@@ -56,6 +61,8 @@ class InscriptionController extends Controller
         if ($formulaire->isSubmitted() && $formulaire->isValid()) {
 
             $inscription = $formulaire->getData();
+
+            dump($inscription);
             $envoiBDD = $this->getDoctrine()->getManager();
             $envoiBDD->persist($inscription);
             $envoiBDD->flush();
