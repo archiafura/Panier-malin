05503db7716d4282d6634eaa83d85f95897a46dc
Isa Vil <isa.vil@free.fr>
2018-05-29 15:46:09 +0200

    Modification Controller Tableaudeborduser pour insertion bdd

--- src/Entity/Tableaudeborduser.php
+++ src/Entity/Tableaudeborduser.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FormInfosPersoRepository")
  */
-class FormInfosPerso
+class Tableaudeborduser
 {
     /**
      * @ORM\Id()
