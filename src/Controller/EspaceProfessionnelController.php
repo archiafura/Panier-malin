a9373b5d5c3757ba9142189b99d0489f7dc5c90d
darkdadis <buquet.regis@hotmail.fr>
2018-05-30 11:16:50 +0200

    creation controller page perso pro avec les formulaires

---
+++ src/Controller/EspaceProfessionnelController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\EspaceProfessionnel;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class EspaceProfessionnelController extends Controller
+{
+    /**
+     * @Route("/espace/professionnel", name="espace_professionnel")
+     */
+    public function index()
+    {
+        return $this->render('espace_professionnel/index.html.twig', [
+            'controller_name' => 'EspaceProfessionnelController',
+        ]);
+    }
+
+    /**
+     * @Route("/espace/professionnel/formulaire", name="espace_professionnel_formulaire")
+     */
+    public function formulaireAjoutPdt(Request $requete)
+    {
+        $formProduit = new EspaceProfessionnel();
+
+        $formulaireProduit = $this->createFormBuilder($formProduit)
+            ->add('typeProduit', TextType::class, array('label' => 'Type de produit',))
+            ->add('dureeDispo', TextType::class, array('label' => 'Durée de disponibilité',))
+            ->add('horaireDispo', TextType::class, array('label' => 'Horaire de disponibilité'))
+            ->add('conseilUtilisation', TextType::class, array('label' => 'Conseil d\'utilisation'))
+            ->add('imgProduit', TextType::class, array('label' => 'Ajouter une image'))
+            ->add('envoyer', SubmitType::class, array('label' => 'AJOUTER'))
+
+            ->getForm();
+
+        $formulaireProduit->handleRequest($requete);
+
+        if ($formulaireProduit->isSubmitted() && $formulaireProduit->isValid()){
+
+            $formProduit = $formulaireProduit->getData();
+
+            $envoiBDD = $this->getDoctrine()->getManager();
+            $envoiBDD->persist($formProduit);
+            $envoiBDD->flush();
+        }
+
+        return $this->render( 'espace_professionnel/index.html.twig',
+            array(
+                'formulaireProduit' => $formulaireProduit->createView()
+            )
+        );
+
+    }
+
+    public function formulaireAjoutEvent(Request $requete)
+    {
+        $formEvent = new EspaceProfessionnel();
+
+        $formulaireEvent = $this->createFormBuilder($formEvent)
+            ->add('typeEvent', TextType::class, array('label' => 'Type d\'evenement',))
+            ->add('dateEvent', TextType::class, array('label' => 'Date de l\'evenement',))
+            ->add('LieuEvent', TextType::class, array('label' => 'Lieu de l\'evenement',))
+            ->add('envoyer', SubmitType::class, array('label' => 'AJOUTER'))
+
+            ->getForm();
+
+        $formulaireEvent->handleRequest($requete);
+
+        if ($formulaireEvent->isSubmitted() && $formulaireEvent->isValid()){
+
+            $formEvent = $formulaireEvent->getData();
+
+            $envoiBDD = $this->getDoctrine()->getManager();
+            $envoiBDD->persist($formEvent);
+            $envoiBDD->flush();
+        }
+
+        return $this->render(
+            'espace_professionnel/index.html.twig',
+            array(
+                'formulaireEvent' => $formulaireEvent->createView()
+            )
+        );
+    }
+}
