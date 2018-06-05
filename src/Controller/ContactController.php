56478c6f06a0d86559098a558e5657cd40b412c5
Bob Marley <bob@marley.com>
2018-06-05 16:41:52 +0200

    Mise en forme css de la page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -63,4 +63,37 @@ class ContactController extends Controller
         return $this->render('contact/contactformOK.html.twig');
     }
 
+
+
+
+    public function index($name, \Swift_Mailer $mailer)
+    {
+        $message = (new \Swift_Message('Hello Email'))
+            ->setFrom('send@example.com')
+            ->setTo('recipient@example.com')
+            ->setBody(
+                $this->renderView(
+                // templates/emails/registration.html.twig
+                    'emails/registration.html.twig',
+                    array('name' => $name)
+                ),
+                'text/html'
+            )
+            /*
+             * If you also want to include a plaintext version of the message
+            ->addPart(
+                $this->renderView(
+                    'emails/registration.txt.twig',
+                    array('name' => $name)
+                ),
+                'text/plain'
+            )
+            */
+        ;
+
+        $mailer->send($message);
+
+        return $this->render(...);
+    }
+
 }
